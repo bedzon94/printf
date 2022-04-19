aa587bfc5484f5de9d9340fcee184a4708cc8d42
“Norris” <bedzon@ieee.org>
2022-04-19 08:14:10 -0400

    Compiled

--- _printstring.c
+++ _printstring.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  *_printstring - function that print a string
- *@s: pointer to value of each simbol of string
+ *@S: pointer to value of each simbol of string
  *Return: counter.
  */
-int _printstring(char *s)
+int _printstring(char *S)
 {
 	int i = 0;
 
-	while (*s != '\0')
+	while (*S != '\0')
 	{
-		_putchar(*s);
-		s++;
+		_putchar(*S);
+		S++;
 		i++;
 	}
 	return (i);
